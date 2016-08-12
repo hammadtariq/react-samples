9f2cd132e6b4edd5504fa5c052614bcd5cefe1fc
Hammad Tariq <hammadtariq65@gmail.com>
2016-08-12 17:22:23 +0500

    names bugs fixes

--- todo-dashboard/app/KanbanBoardContainer.js
+++ todo-dashboard/app/KanbanBoardContainer.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import DashBoard from './DashBoard';
+import KanbanBoard from './KanbanBoard';
 import 'whatwg-fetch';
  
 // If you're running the server locally, the URL will be, by default, localhost:3000
 // Also, the local server doesn't need an authorization header.
-const API_URL = 'http://localhost:3000';
+const API_URL = 'http://kanbanapi.pro-react.com';
 const API_HEADERS = {
-    'Content-Type': 'application/json'
-    //Authorization: 'any-string-you-like'// The Authorization is not needed for local server
+    'Content-Type': 'application/json',
+    Authorization: 'any-string-you-like'// The Authorization is not needed for local server
 };
  
-class DashboardContainer extends Component {
+class KanbanBoardContainer extends Component {
     constructor(){
         super(...arguments);
         this.state = {
-            cards: {active:[],failed:[],complete:[],delayed:[]}
+            cards: []
         };
     }
  
     componentDidMount(){
-        fetch(API_URL+'/logs', {headers: API_HEADERS})
+        fetch(API_URL+'/cards', {headers: API_HEADERS})
             .then((response) => response.json())
             .then((responseData) => {
-                console.log("res data from api: ",responseData)
                 this.setState({cards: responseData});
             })
             .catch((error) => {
@@ -31,8 +30,8 @@ class DashboardContainer extends Component {
     }
     
     render() {
-        return <DashBoard cards={this.state.cards} />
+        return <KanbanBoard cards={this.state.cards} />
     }
 }
  
-export default DashboardContainer;
+export default KanbanBoardContainer;
--- todo-dashboard/app/main.js
+++ todo-dashboard/app/main.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import {render} from 'react-dom';
-import KanbanBoard from './KanbanBoard';
+import KanbanBoardContainer from './KanbanBoardContainer';
 
-let cardsList = [
-{
-  id: 1,
-  title: "Read the Book",
-  description: "I should read the whole book",
-  status: "in-progress",
-  color: '#BD8D31',
-  tasks: []
-},
-{
-  id: 2,
-  title: "Write some code",
-  description: "Code along with the samples in the book The complete source can be found at [github](https://github.com/pro-react)",
-  status: "todo",
-  color: '#3A7E28',
-  tasks: [
-    {
-      id: 1,
-      name: "ContactList Example",
-      done: true
-    },
-    {
-      id: 2,
-      name: "Kanban Example",
-      done: false
-    },
-    {
-      id: 3,
-      name: "My own experiments",
-      done: false
-    }
-  ]
-},
-];
+// let cardsList = [
+// {
+//   id: 1,
+//   title: "Read the Book",
+//   description: "I should read the whole book",
+//   status: "in-progress",
+//   color: '#BD8D31',
+//   tasks: []
+// },
+// {
+//   id: 2,
+//   title: "Write some code",
+//   description: "Code along with the samples in the book The complete source can be found at [github](https://github.com/pro-react)",
+//   status: "todo",
+//   color: '#3A7E28',
+//   tasks: [
+//     {
+//       id: 1,
+//       name: "ContactList Example",
+//       done: true
+//     },
+//     {
+//       id: 2,
+//       name: "Kanban Example",
+//       done: false
+//     },
+//     {
+//       id: 3,
+//       name: "My own experiments",
+//       done: false
+//     }
+//   ]
+// },
+// ];
  
-render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
+render(<KanbanBoardContainer/>, document.getElementById('root'));
